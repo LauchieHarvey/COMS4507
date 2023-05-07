3bdd247dbdd0a71f704e6d5db51fd00904b9b265
Redwallfred <redwallfred@gmail.com>
2023-05-07 18:26:04 +1000

    Added functionality for DOGE :)

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ async function checkBTCAbuse(address) {
   const response = await fetch(`${url}?address=${address}&api_token=${api_key}`);
   const data = await response.json();
 
-  console.log(JSON.stringify(data, null, 4));
+  //console.log(JSON.stringify(data, null, 4));
 
   return data.count > 0;
 }
@@ -54,7 +54,7 @@ async function getBTCTransactionData(tx_hash) {
 
     if (output.spending_outpoints.length !== 0) {
       tx_out.spent = true;
-      tx_out.spend_tx = output.spending_outpoints[0].tx_index;
+      tx_out.spend_tx = output.spending_outpoints[0].tx_index; //Need to update to handle multiple spendings --------------
     }
 
     tx.outputs.push(tx_out);
@@ -63,10 +63,64 @@ async function getBTCTransactionData(tx_hash) {
   tx.value = total_val;
 
   console.log(JSON.stringify(tx, null, 4));
+  return tx;
 }
 
 
-async function getETHTransactionData(txId) {
-  
+async function getDOGETransactionData(hash) {
+  const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+  const url = `https://dogechain.info/api/v1/transaction/${hash}`;
+  const response = await fetch(proxyUrl + url);
+  const data = await response.json();
+
+
+  //Output formatted data
+  //print(json.dumps(data, indent=4))
+  //print("                   #######################                           ")
+
+
+  const tx = {};
+
+  tx.hash = data.transaction.hash;
+  tx.time = data.transaction.time;
+  tx.num_in = data.transaction.inputs_n;
+  tx.num_out = data.transaction.outputs_n //May do different
+  //tx["tx_index"] = data["tx_index"]
+  tx.inputs = [];
+  tx.outputs = [];
+
+  var total_val = 0;
+  for (let i = 0; i < data.transaction.inputs.length; i++) {
+        input = data.transaction.inputs[i];
+        tx_in = {};
+        tx_in.value = input.value;
+        tx_in.tx_index = input.previous_output.hash;  //HASH of the trans where it came from
+        total_val += parseFloat(tx_in.value);
+        tx_in.where = input.address; //Who it came from
+        
+      
+        tx.inputs.push(tx_in);
+  }
+
+  for(let i = 0; i < data.transaction.outputs.length; i++) {
+    output = data.transaction.outputs[i];
+    tx_out = {};
+
+    tx_out.value = output.value;
+    tx_out.tx_index = tx.hash;
+    tx_out.where = output.address;
+    
+    if (output.spent != null) {
+        tx_out.spent = true;
+        tx_out.spend_tx = output.spent.hash;
+    }
+
+    tx.outputs.push(tx_out);
+  }
+
+  tx.value = total_val;
+
+  console.log(JSON.stringify(tx, null, 4));
+  return tx;
 }
 
