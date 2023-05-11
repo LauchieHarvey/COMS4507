2a585358ec169323c4172b13009e76de9ed0c901
Lauchie Harvey <lauchie.harvey@appliedmining.tech>
2023-05-11 23:12:47 +1000

    Added wallet visualisation proof of concept.

--- src/Components/Visualisation/WalletVisualisation.tsx
+++ src/Components/Visualisation/WalletVisualisation.tsx
@@ -1,4 +1,5 @@
-import { Typography } from '@mui/material';
+import { Box } from '@mui/material';
+import Graph from 'react-graph-vis';
 import React from 'react';
 
 interface WalletVisualisationProps {
@@ -7,12 +8,28 @@ interface WalletVisualisationProps {
 }
 
 const WalletVisualisation = ({addrHash, loadAddrData}: WalletVisualisationProps) => {
+    const graph = {
+        nodes: [
+            { id: 1, label: "Node 1\nvalue: 50", title: "node 1 tootip text" },
+            { id: 2, label: "Node 2", title: "node 2 tootip text" },
+            { id: 3, label: "Node 3", title: "node 3 tootip text" },
+            { id: 4, label: "Node 4", title: "node 4 tootip text" },
+            { id: 5, label: "Node 5", title: "node 5 tootip text" }
+        ],
+        edges: [
+            { from: 1, to: 2 },
+            { from: 1, to: 3 },
+            { from: 4, to: 1 },
+            { from: 5, to: 1 }  
+        ]
+    }
+    const options = {}
 
 
     return (
-        <Typography variant="h1" component="div" sx={{ flexGrow: 1, fontSize: '1.5rem'}}>
-            The current hash is {addrHash}
-        </Typography>
+        <Box height="100vh">
+            <Graph graph={graph} options={options}></Graph>
+        </Box>
     );
 }
 
